00f8cc8125df23f035476c5736265129dad80c30
Miguel Eduardo Osorio Montilla <mosoriom@psl.com.co>
2016-09-06 14:55:37 -0500

    added service and google maps

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,37 +1,51 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import Ember from 'ember';
+
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
 
 moduleForAcceptance('Acceptance | list-rentals');
 
 test('should list available rentals.', function (assert) {
-    visit('/');
-    andThen(function(){
-        assert.equal(find('.listing').lenght, 3, 'should see 3 listings');
-    });
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
 });
 
 test('should link to information about the company.', function (assert) {
-    visit('/');
-    click('a:contains("About")');
-    andThen(function() {
-        assert.equal(currentURL(), '/about', 'should navigate to about');
-    });
+  visit('/');
+  click('a:contains("About")');
+  andThen(function () {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
 });
 
-test('should link to contact information.', function (assert) {
-    visit('/');
-    click('a:contains("Contact")');
-    andThen(function() {
-        assert.equal(currentURL(), '/contact', 'should navigate to contact');
-    });
+test('should link to contact information', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function () {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 });
 
 test('should filter the list of rentals by city.', function (assert) {
-    visit('/');
-    fillIn('.list-filter input', 'seattle');
-    keyEvent('.list-filter input', 'keyup', 69);
-    andThen(function() {
-        assert.equal(find('.listing').lenght, 1, 'should show 1 listing');
-        assert.equal(find('.listing .localtion:contains("Seattle")').lenght, 1, 'should contain 1 listing with location Seattle');
-    });
+  visit('/');
+  fillIn('.list-filter input', 'seattle');
+  keyEvent('.list-filter input', 'keyup', 69);
+  andThen(function () {
+    assert.equal(find('.listing').length, 1, 'should show 1 listing');
+    assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
+  });
 });
